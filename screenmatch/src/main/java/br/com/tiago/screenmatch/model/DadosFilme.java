e6a461a6d39aafe6bb0acdc7f0834dbdd5cadb0c
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2024-11-07 00:27:55 -0300

    Criado classe e interface

---
+++ screenmatch/src/main/java/br/com/tiago/screenmatch/model/DadosFilme.java
@@ -0,0 +1,8 @@
+package br.com.tiago.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosFilme(@JsonAlias("name") String nome){ }
+
