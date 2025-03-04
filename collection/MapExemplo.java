57fb192823db04bc0be9fcd12346be9693b8febe
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2025-03-04 13:03:40 -0300

    feat: criado nova pasta

---
+++ collection/MapExemplo.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExemplo {
+    public static void main(String[] args) {
+        Map<Integer, String> mapa = new HashMap<>();
+
+        mapa.put(1, " carro");
+        mapa.put(2, " moto");
+
+        System.out.println(mapa.get(1));
+    }
+}
