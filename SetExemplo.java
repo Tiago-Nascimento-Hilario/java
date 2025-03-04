c8762f8ad027280181a5485c790c76eabb2c0b81
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2025-03-04 12:17:40 -0300

    feat: collections Set

---
+++ SetExemplo.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExemplo {
+    public static void main(String[] args) {
+        Set<String> lista = new HashSet<>();
+        lista.add("Bola");
+        lista.add("lápis");
+
+
+        System.out.println(lista);
+    }
+}
